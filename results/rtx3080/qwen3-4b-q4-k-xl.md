bd8c09e6263125de1845f26115cf36d320b56ffa
Jared Chmielecki <jdchmiel@hotmail.com>
2026-01-18 11:50:54 -0500

    add qwen3 4b results

---
+++ results/rtx3080/qwen3-4b-q4-k-xl.md
@@ -0,0 +1,5 @@
+894 decisions made in 8.67 seconds.
+103.1288978851716 Decisions per second
+206.2577957703432 Tokens generated per second
+1526.5153307768185 Tokens prompt processed per second
+
---
+++ results/rtx3080/run_4bit.sh
@@ -0,0 +1,15 @@
+/AI/llama.cpp/build/bin/llama-server -m /AI/qwen/Qwen3-4B-Instruct-2507-UD-Q4_K_XL.gguf \
+-c 64000 \
+-ctk q8_0 \
+-ctv q8_0 \
+-ngl 99 \
+--min-p 0 \
+--top-k 20 \
+--top-p 0.8 \
+--temp 0.7 \
+--host 0.0.0.0 \
+--port 8080 \
+-fa 1 \
+-np 256
+
+#177
